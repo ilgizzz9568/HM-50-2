5a20a3c8612be296b8bbbd6901b278298d4afbdf
marselev ilgiz <marselevilgiz56@gmail.com>
2025-02-15 14:20:50 +0600

    HM3 , create table

--- handlers/FSMstore.py
+++ handlers/FSMstore.py
@@ -4,17 +4,19 @@ from aiogram.dispatcher.filters import Text, state
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-class FSM_regi(StatesGroup):
+class FSM_reg(StatesGroup):
     model = State()
     size = State()
     category = State()
     price = State()
     photo = State()
     submit = State()
+    productid = State()
+    infoproductid = State()
 
 
 async def start_fsm_regi(message: types.Message):
-    await FSM_regi.model.set()
+    await FSM_reg.model.set()
     await message.answer('Введите модель обуви:')
 
 
@@ -23,7 +25,7 @@ async def load_model(message: types.Message, state: FSMContext):
         data['model'] = message.text
 
 
-    await FSM_regi.next()
+    await FSM_reg.next()
     await message.answer('Укажите размер обуви:')
 
 
@@ -32,14 +34,14 @@ async def load_size(message: types.Message, state: FSMContext):
         data['size'] = message.text
 
 
-    await FSM_regi.next()
+    await FSM_reg.next()
     await message.answer('Укажите категорию:')
 
 async def load_category(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['category'] = message.text
 
-    await FSM_regi.next()
+    await FSM_reg.next()
     await message.answer('Укажите стоимость:')
 
 async def load_price(message: types.Message, state: FSMContext):
@@ -47,14 +49,14 @@ async def load_price(message: types.Message, state: FSMContext):
         data['price'] = message.text
 
 
-    await FSM_regi.next()
+    await FSM_reg.next()
     await message.answer('Отправьте фото:')
 
 async def load_photo(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['photo'] = message.photo[-1].file_id
 
-    await FSM_regi.next()
+    await FSM_reg.next()
     await message.answer('Верны ли данные о товаре?:')
     await message.answer_photo(photo=data['photo'],
                                caption=f'model - {data["model"]}\n'
@@ -80,12 +82,12 @@ async def submit(message: types.Message, state: FSMContext):
 
 def register_handlers_fsm(dp: Dispatcher):
     dp.register_message_handler(start_fsm_regi, commands=['regis'])
-    dp.register_message_handler(load_model,state=FSM_regi.model)
-    dp.register_message_handler(load_size,state=FSM_regi.size)
-    dp.register_message_handler(load_category,state=FSM_regi.category)
-    dp.register_message_handler(load_price,state=FSM_regi.price)
-    dp.register_message_handler(load_photo,state=FSM_regi.photo, content_types=['photo'])
-    dp.register_message_handler(submit,state=FSM_regi.submit)
+    dp.register_message_handler(load_model,state=FSM_reg.model)
+    dp.register_message_handler(load_size,state=FSM_reg.size)
+    dp.register_message_handler(load_category,state=FSM_reg.category)
+    dp.register_message_handler(load_price,state=FSM_reg.price)
+    dp.register_message_handler(load_photo,state=FSM_reg.photo, content_types=['photo'])
+    dp.register_message_handler(submit,state=FSM_reg.submit)
 
 
 
